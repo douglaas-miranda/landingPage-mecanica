30a877a15c9de49b9b7f9116edd2e67fb43b9f14
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-03-12 14:35:02 -0300

    Adicionando footer e mapa, e um arquivo js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggle () {
+    var div = document.getElementById("mapa");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
